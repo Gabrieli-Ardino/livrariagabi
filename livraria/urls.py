057645f86f8eaea3cc5d219a1185e795a4f46c25
= <=>
2022-08-30 16:28:38 -0300

    first commit

---
+++ livraria/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import include, path
+
+from rest_framework.routers import DefaultRouter
+
+from core.views import CategoriaViewSet, EditoraViewSet, LivroViewSet
+router = DefaultRouter()
+router.register(r'categorias', CategoriaViewSet)
+router.register(r'editoras', EditoraViewSet)
+router.register(r'livros', LivroViewSet)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+]
