9577cb31c4790d89bb2701fab3b052acc62f8d2b
gabrieliardino@gmail.com <erico.pieri@gmail.com>
2022-11-30 12:33:38 -0300

    aula 13

--- livraria/settings.py
+++ livraria/settings.py
@@ -54,6 +54,12 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
+REST_FRAMEWORK = {
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.DjangoModelPermissions",
+    ],
+}
+
 ROOT_URLCONF = "livraria.urls"
 
 TEMPLATES = [
